49b18ae6feb623474175f234d5aa9d60e629f328
marym <maria_manzhos@tut.by>
2021-07-08 09:28:43 +0300

    fixed task_2, part_2

--- HomeWork_7/HomeWork_Lesson_8_Part_2/HomeWork_Lesson_8_Part_2/ViewController2.swift
+++ HomeWork_7/HomeWork_Lesson_8_Part_2/HomeWork_Lesson_8_Part_2/ViewController2.swift
@@ -9,41 +9,34 @@ import UIKit
 
 class ViewController2: UIViewController {
     
-    var whiteView: UIView = UIView()
-    var blackView: UIView = UIView()
+    var currentView: UIView = UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor(red: 145 / 255, green: 180 / 255, blue: 180 / 255, alpha: 1.0)
-        
+    
         var y = 50
         var x = 50
 
         for i in 1...8 {
 
             x = 50
-
-            if i % 2 != 0 {
-                for _ in 1...4 {
-                    whiteView = UIView(frame: CGRect(x: x, y: y, width: 30, height: 30))
-                    whiteView.backgroundColor = UIColor.white
-                    view.addSubview(whiteView)
-                    blackView = UIView(frame: CGRect(x: x + 30, y: y, width: 30, height: 30))
-                    blackView.backgroundColor = UIColor.black
-                    view.addSubview(blackView)
-                    x += 60
+            
+            for j in 1...8 {
+                
+                currentView = UIView(frame: CGRect(x: x, y: y, width: 30, height: 30))
+                
+                if (i + j) % 2 != 0 {
+                    currentView.backgroundColor = UIColor.white
+                } else {
+                    currentView.backgroundColor = UIColor.black
                 }
-            } else {
-                for _ in 1...4 {
-                    blackView = UIView(frame: CGRect(x: x, y: y, width: 30, height: 30))
-                    blackView.backgroundColor = UIColor.black
-                    view.addSubview(blackView)
-                    whiteView = UIView(frame: CGRect(x: x + 30, y: y, width: 30, height: 30))
-                    whiteView.backgroundColor = UIColor.white
-                    view.addSubview(whiteView)
-                    x += 60
-                }
+                
+                view.addSubview(currentView)
+                
+                x += 30
+                
             }
 
             y += 30
