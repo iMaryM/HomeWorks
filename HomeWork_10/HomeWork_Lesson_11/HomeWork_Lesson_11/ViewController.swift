676fda2bbb14bf230c5833a1459e5c85a792fcd7
marym <maria_manzhos@tut.by>
2021-07-21 14:48:10 +0300

    added rounded button

--- HomeWork_10/HomeWork_Lesson_11/HomeWork_Lesson_11/ViewController.swift
+++ HomeWork_10/HomeWork_Lesson_11/HomeWork_Lesson_11/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var roundedButton: [UIButton]!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+        
 
+    }
+    
+    override func viewWillAppear(_ : Bool) {
+        
+        super.viewWillAppear(true)
+        
+        for value in roundedButton {
+            value.layer.cornerRadius = value.bounds.size.width / 2.0
+            value.clipsToBounds = true
+        }
+    }
 
 }
 
