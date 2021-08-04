f3d72a1838a16ba50a41553b853fac54091940f7
marym <maria_manzhos@tut.by>
2021-08-04 22:54:22 +0300

    refactoring code in func viewDidLoad() fixed bug

--- HomeWork_12/HomeWork_Lesson_13/HomeWork_Lesson_13/ViewControllers/CheckersViewController.swift
+++ HomeWork_12/HomeWork_Lesson_13/HomeWork_Lesson_13/ViewControllers/CheckersViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class CheckersViewController: UIViewController {
-    
+
     var checkerBoardBoarder = UIView()
     var checkerBoard = UIView()
     var arrayOfCheckerViews = [UIImageView]()
@@ -17,31 +17,97 @@ class CheckersViewController: UIViewController {
     var timerLabel = UILabel()
     var timer: Timer = Timer()
     var count: Int = 0
-    var isTap = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let screenSize = UIScreen.main.bounds
         
-        let timerViewHeight = ((screenSize.height / 2) - ((screenSize.width - 12) / 2) - 48 - (screenSize.height * 0.05))
-        let timerOriginY = (48 + (0.05 * screenSize.height))
-        
-        let timerView = UIView(frame: CGRect(x: 6, y: timerOriginY, width: screenSize.width - 12, height: timerViewHeight))
-        timerView.backgroundColor = .none
-        view.addSubview(timerView)
+        createTimer(screenSize: screenSize)
 
-        timerLabel = UILabel(frame: CGRect(x: ((screenSize.width - 12) / 2), y: 0, width: ((screenSize.width - 12) / 2), height: timerViewHeight))
-        timerLabel.text = "00 : 00 : 00"
-        timerView.addSubview(timerLabel)
+        createCheckerBoard(screenSize: screenSize)
+
+    }
+    
+    @IBAction func backToMenu(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+        timer.invalidate()
+    }
+    
+    @objc
+    func tapGestureAction(_ sender: UILongPressGestureRecognizer) {
+        switch sender.state {
+        case .began:
+            guard let currentChecker = sender.view else { return } //определяем шашку которую двигаем
+            UIView.animate(withDuration: 0.3) {
+                currentChecker.transform = currentChecker.transform.scaledBy(x: 1.2, y: 1.2)
+            }
+        case .ended:
+            guard let currentChecker = sender.view else { return } //определяем шашку которую двигаем
+            UIView.animate(withDuration: 0.3) {
+                currentChecker.transform = .identity
+            }
+        default:
+            break
+        }
+    }
+    
+    @objc
+    func panGestureAction(_ sender: UIPanGestureRecognizer) {
         
-        let timerImage = UIImageView(frame: CGRect(x: ((timerView.frame.width / 2) - timerView.frame.height), y: timerView.frame.height / 4, width: timerView.frame.height, height: timerView.frame.height / 2))
-        timerImage.image = UIImage(named: "timer")
-        timerImage.contentMode = .scaleAspectFit
-        timerView.addSubview(timerImage)
+        let location = sender.location(in: checkerBoard)
         
-        timer = Timer(timeInterval: 1, target: self, selector: #selector(timerCounter), userInfo: nil, repeats: true)
-        RunLoop.main.add(timer, forMode: .common)
+        let translation = sender.translation(in: checkerBoard)
+        
+        switch sender.state {
+        case .changed:
+            guard let currentView = sender.view?.superview, //определяем клеточку на которой стоит шашка
+                  let currentChecker = sender.view, //определяем шашку которую двигаем
+                  let defaultOrigin = sender.view?.frame.origin else { //определяем координаты шашки которую будем двигать
+                return
+            }
+            
+            checkerBoard.bringSubviewToFront(currentView) //передвигаем клеточку на передний план
+            currentChecker.frame.origin = CGPoint(x: defaultOrigin.x + translation.x, y: defaultOrigin.y + translation.y) //передвигаем шашку относительно точки с которой сдвигаем
+            sender.setTranslation(.zero, in: checkerBoard) //сбрасываем translation, чтобы всегда сдвиг был с нуля
+            
+        case .ended:
+            var newCheckerView: UIView? = nil //клеточка в которую хотим поставить шашку
+            //проверяем является ли клетка в которую хотим поставить шашку черной
+            for value in arrayOfCheckerViews {
+                if value.frame.contains(location), value.tag == 0 {
+                    newCheckerView = value
+                }
+            }
+
+            sender.view?.frame.origin = CGPoint(x: 0, y: 0) // сбрасываем позицию на 5;5 чтобы отцентрировать
+            
+            //проверяем является есть ли в клеточке в которую хотим поставить шашку другая шашка
+            guard let newCheckerView = newCheckerView, newCheckerView.subviews.isEmpty,
+                  let checker = sender.view else { return }
+            
+            newCheckerView.addSubview(checker) // добавляем шашку на новую клетку
+            
+            guard let currentChecker = sender.view else { return } //определяем шашку которую двигаем
+            UIView.animate(withDuration: 0.3) {
+                currentChecker.transform = .identity
+            }
+            
+        default:
+            break
+        }
+    }
+    
+    @objc
+    func timerCounter () {
+        count += 1
+        let time = calculateTime(seconds: count)
+        let timeString = convertToTimeString(hours: time.0, minutes: time.1, seconds: time.2)
+        timerLabel.text = timeString
+        
+    }
+    
+    func createCheckerBoard(screenSize: CGRect) {
         
         checkerBoardBoarder = UIView(frame: CGRect(origin: .zero, size: CGSize(width: screenSize.width - 12, height: screenSize.width - 12)))
         checkerBoardBoarder.center = view.center
@@ -94,94 +160,32 @@ class CheckersViewController: UIViewController {
         }
     }
     
-    @IBAction func backToMenu(_ sender: Any) {
-        navigationController?.popViewController(animated: true)
-        timer.invalidate()
-    }
-    
-    @objc
-    func tapGestureAction(_ sender: UILongPressGestureRecognizer) {
-        switch sender.state {
-        case .changed:
-            guard !isTap else { return }
-            isTap = true
-            guard let currentChecker = sender.view else { return } //определяем шашку которую двигаем
-            UIView.animate(withDuration: 0.3) {
-                currentChecker.transform = currentChecker.transform.scaledBy(x: 1.2, y: 1.2)
-            }
-        case .ended:
-            guard let currentChecker = sender.view else { return } //определяем шашку которую двигаем
-            UIView.animate(withDuration: 0.3) {
-                currentChecker.transform = .identity
-            }
-        default:
-            break
-        }
-    }
-    
-    @objc
-    func panGestureAction(_ sender: UIPanGestureRecognizer) {
-        guard isTap else { return }
+    func createTimer(screenSize: CGRect) {
+        let timerViewHeight = ((screenSize.height / 2) - ((screenSize.width - 12) / 2) - 48 - (screenSize.height * 0.05))
+        let timerOriginY = (48 + (0.05 * screenSize.height))
+        
+        let timerView = UIView(frame: CGRect(x: 6, y: timerOriginY, width: screenSize.width - 12, height: timerViewHeight))
+        timerView.backgroundColor = .none
+        view.addSubview(timerView)
 
-        let location = sender.location(in: checkerBoard)
+        timerLabel = UILabel(frame: CGRect(x: ((screenSize.width - 12) / 2), y: 0, width: ((screenSize.width - 12) / 2), height: timerViewHeight))
+        timerLabel.text = "00 : 00 : 00"
+        timerView.addSubview(timerLabel)
         
-        let translation = sender.translation(in: checkerBoard)
+        let timerImage = UIImageView(frame: CGRect(x: ((timerView.frame.width / 2) - timerView.frame.height), y: timerView.frame.height / 4, width: timerView.frame.height, height: timerView.frame.height / 2))
+        timerImage.image = UIImage(named: "timer")
+        timerImage.contentMode = .scaleAspectFit
+        timerView.addSubview(timerImage)
         
-        switch sender.state {
-        case .changed:
-            guard let currentView = sender.view?.superview, //определяем клеточку на которой стоит шашка
-                  let currentChecker = sender.view, //определяем шашку которую двигаем
-                  let defaultOrigin = sender.view?.frame.origin else { //определяем координаты шашки которую будем двигать
-                return
-            }
-            
-            checkerBoard.bringSubviewToFront(currentView) //передвигаем клеточку на передний план
-            currentChecker.frame.origin = CGPoint(x: defaultOrigin.x + translation.x, y: defaultOrigin.y + translation.y) //передвигаем шашку относительно точки с которой сдвигаем
-            sender.setTranslation(.zero, in: checkerBoard) //сбрасываем translation, чтобы всегда сдвиг был с нуля
-            
-        case .ended:
-            var newCheckerView: UIView? = nil //клеточка в которую хотим поставить шашку
-            //проверяем является ли клетка в которую хотим поставить шашку черной
-            for value in arrayOfCheckerViews {
-                if value.frame.contains(location), value.tag == 0 {
-                    newCheckerView = value
-                }
-            }
-
-            sender.view?.frame.origin = CGPoint(x: 0, y: 0) // сбрасываем позицию на 5;5 чтобы отцентрировать
-            
-            //проверяем является есть ли в клеточке в которую хотим поставить шашку другая шашка
-            guard let newCheckerView = newCheckerView, newCheckerView.subviews.isEmpty,
-                  let checker = sender.view else { return }
-            
-            newCheckerView.addSubview(checker) // добавляем шашку на новую клетку
-            
-            guard let currentChecker = sender.view else { return } //определяем шашку которую двигаем
-            UIView.animate(withDuration: 0.3) {
-                currentChecker.transform = .identity
-            }
-        
-            isTap = false
-            
-        default:
-            break
-        }
+        timer = Timer(timeInterval: 1, target: self, selector: #selector(timerCounter), userInfo: nil, repeats: true)
+        RunLoop.main.add(timer, forMode: .common)
     }
     
-    @objc
-    func timerCounter () {
-        count += 1
-        let time = calculateTime(seconds: count)
-        let timeString = convertToTimeString(hours: time.0, minutes: time.1, seconds: time.2)
-        timerLabel.text = timeString
-        
-    }
-    
-    func calculateTime (seconds: Int) -> (Int, Int, Int) {
+    func calculateTime(seconds: Int) -> (Int, Int, Int) {
         return ((seconds / 3600), ((seconds % 3600) / 60), ((seconds % 3600) % 60))
     }
     
-    func convertToTimeString (hours: Int, minutes: Int, seconds: Int) -> String {
+    func convertToTimeString(hours: Int, minutes: Int, seconds: Int) -> String {
         var timeString = ""
         timeString += "\(hours)"
         timeString += " : "
